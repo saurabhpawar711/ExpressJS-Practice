812c2c088b1ebee891e5ae14b409e7a371dcac79
saurabhpawar711 <saurabhpawar1107@gmail.com>
2023-06-18 11:11:02 +0530

    Intro to Middlewares

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 const express = require("express")
 const http = require('http')
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use((req,res,next) => {
-    next();
+app.use(bodyParser.urlencoded({extended : false}))
+
+app.use('/addProducts', (req,res,next) => {
+    res.send("<form action='/products' method='POST'><input type='text' name='title'><input type='text' name='description'><button type='submit'>Add product</button></form>")
 })
 
-app.use((req,res,next) => {
-    // res.send("<h1>Hello from Express JS</h1>")
-    res.send({name: 'John', age: 30})
+app.use('/products', (req,res,next) => {
+    console.log(req.body)
+    res.redirect('/')
 })
+
+app.use('/', (req,res,next) => {
+   res.send("<h1>Hello from Express JS</h1>")
+})
+
 const server = http.createServer(app);
 server.listen(3000)
